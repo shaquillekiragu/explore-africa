b558231c7fdeb9deea3ffd1523ec924aa9375c7e
Shaquille Kiragu <skiragu77@gmail.com>
2024-05-03 17:15:09 +0100

    Setting up repo 2

---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <h1>The Africa Encyclopedia</h1>;
+}
+
+export default Header;
---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <>
+      <nav>
+        <Link />
+      </nav>
+      <p>Welcome to the Encylopedia of the African Continent!</p>
+    </>
+  );
+}
+
+export default Home;
